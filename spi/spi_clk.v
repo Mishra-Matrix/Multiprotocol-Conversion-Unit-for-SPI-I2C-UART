2ceca39a9f9a89b684bab1497216820789c1e3e8
Mishra-Matrix <engineeringakm181@gmail.com>
2026-01-20 12:34:01 +0530

    Add reset functionality to spi_clk_divider module

--- spi/spi_clk.v
+++ spi/spi_clk.v
@@ -1,16 +1,18 @@
 `timescale 1ns / 1ps
-
 module spi_clk_divider #(
     parameter DIVISOR = 50
 )(
     input  wire clk,
+    input  wire rst,
     output reg  spi_clk_en
 );
-
-    reg [15:0] count = 0;
+    reg [$clog2(DIVISOR)-1:0] count;
 
     always @(posedge clk) begin
-        if(count == DIVISOR-1) begin
+        if (rst) begin
+            count <= 0;
+            spi_clk_en <= 0;
+        end else if (count == DIVISOR-1) begin
             count <= 0;
             spi_clk_en <= 1;
         end else begin
@@ -18,5 +20,4 @@ module spi_clk_divider #(
             spi_clk_en <= 0;
         end
     end
-
 endmodule
